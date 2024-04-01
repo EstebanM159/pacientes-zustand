1d6b4003672d52605b809f9e99b3bc8e6a825738
EstebanM159 <estebanm91231@gmail.com>
2024-04-01 18:28:14 -0300

    Create project with vite/react/tailwind

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    ' @typescript-eslint/no-misused-promises': 'off'
+    // 'react/prop-types': 'off'
+  }
+}
