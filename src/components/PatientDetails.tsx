087ded897f717ba23653538b9c8b88f560420154
EstebanM159 <estebanm91231@gmail.com>
2024-04-02 11:27:51 -0300

    Add buttons

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,9 +1,11 @@
+import { usePatientStore } from '../store'
 import { type Patient } from '../types'
 import PatientsDetailItem from './PatientsDetailItem'
 type PatientDetailsProps = {
   patient: Patient
 }
 export default function PatientDetails ({ patient }: PatientDetailsProps) {
+  const { deletePatient, setActiveId } = usePatientStore()
   return (
     <div className='bg-white mx-5 px-5 my-10 py-10 shadow-md rounded-xl'>
         <PatientsDetailItem label='ID' data={patient.id}/>
@@ -12,6 +14,20 @@ export default function PatientDetails ({ patient }: PatientDetailsProps) {
         <PatientsDetailItem label='Email' data={patient.email}/>
         <PatientsDetailItem label='Fecha' data={patient.date.toString()}/>
         <PatientsDetailItem label='Sintomas' data={patient.symptoms}/>
+        <div className="flex flex-col md:flex-row justify-between gap-3 mt-10">
+            <button
+                type='button'
+                className='py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg'
+                onClick={() => { setActiveId(patient.id) }}
+            >Editar
+            </button>
+            <button
+                type='button'
+                className='py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg'
+                onClick={() => { deletePatient(patient.id) }}
+            >Borrar
+            </button>
+        </div>
     </div>
   )
 }
